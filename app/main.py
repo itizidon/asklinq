ad7d4b263436b3180534634dad3579153806cdee
Don <donfullstack@gmail.com>
2026-05-13 15:21:31 -0500

    adding new seeding

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from app.api.webhooks import router as webhook_router
+from app.core.database import Base, engine
+
 
 app = FastAPI(title="asklinq", version="1.0.0")
 
 app.include_router(webhook_router, prefix="/webhook")
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 @app.get("/")
 async def root():
